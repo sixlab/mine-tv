e926fc663fc5e355a76551c11487434135f38962
nianqinianyi <nianqinianyi@163.com>
2022-01-31 00:57:58 +0800

    新加一个module，开发全新的mine-tv

---
+++ v4app/src/main/java/tech/minesoft/minetv/MainActivity.java
@@ -0,0 +1,31 @@
+package tech.minesoft.minetv;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import tech.minesoft.minetv.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+}
